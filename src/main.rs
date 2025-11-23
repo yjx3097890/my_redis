d9da757f3608b30930b2dd071ce26be237134c82
阎继先 <315644588@qq.com>
2025-11-23 18:07:31 +0800

    my_select

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod my_tokio;
 mod my_tokio_pro;
 mod delay;
 
-use std::time::{Duration, Instant};
+use std::{time::{Duration, Instant}};
 
 use my_tokio_pro::MyTokio;
 use delay::Delay;
@@ -13,7 +13,7 @@ fn main() {
 
     my_tokio.spawn(async {
         let when = Instant::now() + Duration::from_millis(1000);
-        let future = Delay { when };
+        let future = Delay { when, waker: None };
 
         let out = future.await;
 
